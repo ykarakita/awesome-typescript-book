e9bacfc37f3f3a457baede2713eecb4d222a003c
ykarakita <taf.749@gmail.com>
2022-06-04 15:17:41 +0900

    入力した値を受け取って処理する関数作成

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-const sayHello = (name: string) => {
-  return `Hello, ${name}`
+const printLine = (text: string, breakLine: boolean = true) => {
+  process.stdout.write(text + (breakLine ? '\n' : ''))
 }
 
-process.stdout.write(sayHello('Michael Jackson'))
+const promptInput = async (text: string) => {
+  printLine(`\n${text}\n`, false)
+  const input: string = await new Promise((resolve) => {
+    process.stdin.once('data', (data) => resolve(data.toString()))
+  })
+  return input.trim()
+}
+
+;(async () => {
+  const name = await promptInput('名前を入力してください')
+  console.log(name)
+  const age = await promptInput('年齢を入力してください')
+  console.log(age)
+  process.exit()
+})()
