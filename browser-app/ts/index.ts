fff7132c8a7ce9d5a04ae868520bdb7c93ddb4f7
ykarakita <taf.749@gmail.com>
2022-06-05 11:40:26 +0900

    add Application class

--- browser-app/ts/index.ts
+++ browser-app/ts/index.ts
@@ -1,1 +1,10 @@
-console.log('hello')
+class Application {
+  start() {
+    console.log('hello world')
+  }
+}
+
+window.addEventListener('load', () => {
+  const app = new Application()
+  app.start()
+})
