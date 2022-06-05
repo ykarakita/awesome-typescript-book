21c83530279e559b301291655ec8cb6207a1d213
ykarakita <taf.749@gmail.com>
2022-06-05 12:28:15 +0900

    add Task class

--- browser-app/ts/index.ts
+++ browser-app/ts/index.ts
@@ -1,4 +1,5 @@
 import { EventListener } from './EventListener'
+import { Task } from './Task'
 
 class Application {
   private readonly eventListener = new EventListener()
@@ -10,7 +11,13 @@ class Application {
 
   private handleSubmit = (e: Event) => {
     e.preventDefault()
-    console.log('submitted')
+
+    const titleInput = document.getElementById('title') as HTMLInputElement
+
+    if (!titleInput.value) return
+
+    const task = new Task({ title: titleInput.value })
+    console.log(task)
   }
 }
 
