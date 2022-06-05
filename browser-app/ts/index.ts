aa64453712d156118b8ef2014d679f5eb9531162
ykarakita <taf.749@gmail.com>
2022-06-05 11:52:02 +0900

    削除 button に eventListener を追加

--- browser-app/ts/index.ts
+++ browser-app/ts/index.ts
@@ -1,6 +1,13 @@
+import { EventListener } from './EventListener'
+
 class Application {
   start() {
-    console.log('hello world')
+    const eventListener = new EventListener()
+    const button = document.getElementById('deleteAllDoneTask')
+
+    if (!button) return
+
+    eventListener.add('sample', 'click', button, () => alert('clicked'))
   }
 }
 
